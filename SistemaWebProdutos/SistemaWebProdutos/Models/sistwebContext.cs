0c17e40142323e623447067f68fcba4e3da6952a
GabNy <nycollyserafim@gmail.com>
2019-10-21 14:49:32 -0300

    primeira versão
    suhausudsudsu

---
+++ SistemaWebProdutos/SistemaWebProdutos/Models/sistwebContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SistemaWebProdutos.Models
+{
+    public partial class sistwebContext : DbContext
+    {
+        public sistwebContext()
+        {
+        }
+
+        public sistwebContext(DbContextOptions<sistwebContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Produtos> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produtos>(entity =>
+            {
+                entity.HasKey(e => e.ProdCodigo);
+
+                entity.Property(e => e.ProdCodigo).HasColumnName("Prod_Codigo");
+
+                entity.Property(e => e.ProdNome)
+                    .IsRequired()
+                    .HasColumnName("Prod_Nome")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.ProdPreco)
+                    .HasColumnName("Prod_Preco")
+                    .HasColumnType("decimal(14, 2)");
+
+                entity.Property(e => e.ProdQtde).HasColumnName("Prod_Qtde");
+            });
+        }
+    }
+}
